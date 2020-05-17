83448f4379c246057bc09eb31402bb25e8bce4d5
anaherreramiranda <anac3714@hotmail.com>
2020-05-16 20:35:40 -0500

    Update 01.codigoImperativo.php

--- 01.codigoImperativo.php
+++ 01.codigoImperativo.php
@@ -6,6 +6,12 @@ $automovil1 = (object)["marca"=>"Toyota", "modelo"=>"Corolla"];
 $automovil2 = (object)["marca"=>"Hyundai", "modelo"=>"Accent Vision"];
 
 
+function mostrar($automovil){
+
+    echo "<p>HOLA! soy un $automovil->marca, modelo $automovil->modelo</p>";
+
+}
+
 
 
 
