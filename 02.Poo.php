7537d6f2b65cb18f986c11be118bc56d9f70f353
anaherreramiranda <anac3714@hotmail.com>
2020-05-16 21:23:15 -0500

    inicio

---
+++ 02.Poo.php
@@ -0,0 +1,28 @@
+<?php
+
+#CLASE :
+#Una clase es un modelo que se utiliza para crear objetos que comparte un mismo comportamiento, estado e identidad.
+
+class Automovil{
+
+    #PROPIEDADES :
+    #Son las caracteristicas que puede tener un objeto.
+
+    public $marca;
+    public $modelo;
+
+    #MÉTODO :
+    #Es el algoritmo asociado a un objeto que indica la capacidad de lo que este puede hacer. La unica diferencia entre metodo y función, es que llamamos método a las funciones de una clase (en la poo), mientras que llamamos funciones a los aloritmos de la programación estructurada.
+
+    public function mostrar(){
+
+
+
+    }
+
+
+
+
+}
+
+?>
